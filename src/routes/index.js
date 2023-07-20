3ef85ba15f5d353f0b3427d0925a21d6320c7013
ArimMis <Ari_mino@live.fr>
2023-07-20 17:22:13 +0300

    updates

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,7 @@ import MainRoutes from './MainRoutes';
 import { lazy } from 'react';
 const AuthLogin3 = Loadable(lazy(() => import('views/pages/authentication/authentication3/Login3')));
 const AuthRegister3 = Loadable(lazy(() => import('views/pages/authentication/authentication3/Register3')));
+const AuthCode = Loadable(lazy(() => import('views/pages/authentication/AuthCode')));
 // ==============================|| ROUTING RENDER ||============================== //
 export default function ThemeRoutes() {
   return useRoutes([
@@ -17,5 +18,10 @@ export default function ThemeRoutes() {
     {
       path: '/register',
       element: <AuthRegister3 />
-  }]);
+    },
+    {
+      path: '/authCode',
+      element: <AuthCode />
+    }
+]);
 }
