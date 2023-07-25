e09eb09d00ff8a7f2d41f019704cb765277858f8
ArimMis <Ari_mino@live.fr>
2023-07-25 21:37:34 +0300

    offres OK

--- src/views/Offre/Offre.js
+++ src/views/Offre/Offre.js
@@ -11,14 +11,14 @@ import {
   DialogContent,
   DialogTitle,
   Grid,
-  Typography
+  Typography,
+  TextField
 } from '@mui/material';
 import PhoneInput from 'react-phone-input-2';
 import 'react-phone-input-2/lib/material.css';
-import api from 'requests/api';
 import { useEffect } from 'react';
 import ProductPlaceholder from 'ui-component/cards/Skeleton/ProductPlaceholder';
-  
+
 const OfferCard = ({ offer, onSelectOffer }) => {
   const [expanded, setExpanded] = useState(false);
 
@@ -28,7 +28,7 @@ const OfferCard = ({ offer, onSelectOffer }) => {
   };
 
   return (
-    <Card sx={{ maxWidth: 345 }}>
+    <Card sx={{ maxWidth: 345}}>
       <CardActionArea onClick={() => onSelectOffer(offer)}>
         <CardMedia
           component="img"
@@ -42,7 +42,7 @@ const OfferCard = ({ offer, onSelectOffer }) => {
             {offer.nom}
           </Typography>
           <Typography
-            variant="body2"
+            variant="body3"
             color="text.secondary"
             sx={{
               overflow: 'hidden',
@@ -62,6 +62,9 @@ const OfferCard = ({ offer, onSelectOffer }) => {
           <Typography variant="h6" color="primary" sx={{ mt: 2 }}>
             {offer.prix}€
           </Typography>
+          <Typography variant="body2" color="text.secondary" sx={{ mt: 1 }}>
+          {offer.pays}/{offer.operateur}
+          </Typography>
         </CardContent>
       </CardActionArea>
     </Card>
@@ -69,34 +72,77 @@ const OfferCard = ({ offer, onSelectOffer }) => {
 };
 
 
-
 const OfferScreen = () => {
+  const [expanded, setExpanded] = useState(false);
   const [selectedOffer, setSelectedOffer] = useState(null);
   const [phoneNumber, setPhoneNumber] = useState('');
   const [openDialog, setOpenDialog] = useState(false);
-  const [loading, setloading] = useState(false);
-  const [offres , setOffres] = useState([]);
-  const [expanded, setExpanded] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [offres, setOffres] = useState([]);
+  const [filteredOffres, setFilteredOffres] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
+
   const toggleDescription = (event) => {
     event.stopPropagation();
     setExpanded((prevExpanded) => !prevExpanded);
   };
-  const fetchOffres = async () => {
-    setloading(true)
-    api.get('/offres').then((resp) => {
-      console.log(resp.data);
-      setOffres(resp.data);
-      setloading(false)
-    }).catch((err) => {
-      console.log(err.response);
-      setloading(false)
-    })
-  }
+  const fakeOffres = [
+    {
+      id: 1,
+      nom: 'Offre 1',
+      description: 'Description de l\'offre 1 ',
+      prix: 10,
+      pays: 'France',
+      operateur: 'Orange',
+      image: 'https://via.placeholder.com/640x480?text=Offre+1',
+    },{
+      id: 2,
+      nom: 'Offre 2',
+      description: 'Description de l\'offre 1',
+      prix: 12,
+      pays: 'France',
+      operateur: 'Bouygue',
+      image: 'https://via.placeholder.com/640x480?text=Offre+2',
+    },{
+      id: 3,
+      nom: 'Offre 3',
+      description: 'Description de l\'offre 3',
+      prix: 15,
+      pays: 'Mada',
+      operateur: 'Orange',
+      image: 'https://via.placeholder.com/640x480?text=Offre+3',
+    },{
+      id: 4,
+      nom: 'Offre 4',
+      description: 'Description de l\'offre 4',
+      prix: 25,
+      pays: 'Mada',
+      operateur: 'Telma',
+      image: 'https://via.placeholder.com/640x480?text=Offre+4',
+    },{
+      id: 5,
+      nom: 'Offre 5',
+      description: 'Description de l\'offre 5',
+      prix: 19,
+      pays: 'Mali',
+      operateur: 'Malitel',
+      image: 'https://via.placeholder.com/640x480?text=Offre+5',
+    },
+  ];
+  useEffect(() => {
+    // Fetch data from API or use fake data here
+    setLoading(true);
+    // Simulate API call delay
+    setTimeout(() => {
+      setOffres(fakeOffres);
+      setLoading(false);
+    }, 1000);
+  }, []);
 
   useEffect(() => {
-    fetchOffres();
-  },[])
- 
+    filterOffres();
+  }, [searchValue, offres]);
+
   const handleSelectOffer = (offer) => {
     setSelectedOffer(offer);
     setOpenDialog(true);
@@ -115,27 +161,56 @@ const OfferScreen = () => {
     setOpenDialog(false);
   };
 
+ 
+
+  const filterOffres = () => {
+    if (searchValue === '') {
+      setFilteredOffres(offres);
+    } else {
+      const filteredByCountry = offres.filter((offer) =>
+        offer.pays.toLowerCase().includes(searchValue.toLowerCase())
+      );
+      const filteredByOperator = offres.filter((offer) =>
+        offer.operateur.toLowerCase().includes(searchValue.toLowerCase())
+      );
+      const filteredOffers = [...filteredByCountry, ...filteredByOperator];
+      setFilteredOffres(filteredOffers);
+    }
+  };
+  useEffect(() => {
+    filterOffres();
+  }, [searchValue]);
+
   return (
     <Box sx={{ p: 2 }}>
       <Typography variant="h4" sx={{ mb: 2 }}>
         Offres disponibles
       </Typography>
-   { loading?<ProductPlaceholder/>
-       : offres.length === 0 ? 
-        <Grid container spacing={2} sx={{alignItems:"center",justifyContent:"center",display:'flex'}}>
+      <TextField
+        label="Rechercher par pays ou opérateur"
+        variant="outlined"
+        fullWidth
+        value={searchValue}
+        onChange={(e) => setSearchValue(e.target.value)}
+        sx={{ mb: 2 }}
+      />
+      {loading ? (
+        <ProductPlaceholder />
+      ) : filteredOffres.length === 0 ? (
+        <Grid container spacing={2} sx={{ alignItems: 'center', justifyContent: 'center', display: 'flex' }}>
           <Typography variant="h6" sx={{ mb: 2 }}>
             Aucune offre disponible pour le moment
           </Typography>
         </Grid>
-        :
+      ) : (
         <Grid container spacing={2}>
-        {offres.map((offer) => (
-          <Grid item key={offer.id} xs={12} sm={6} md={4}>
-            <OfferCard offer={offer} onSelectOffer={handleSelectOffer} />
-          </Grid>
-        ))}
-      </Grid>
-      }
+          {filteredOffres.map((offer) => (
+            <Grid item key={offer.id} xs={12} sm={6} md={4}>
+              <OfferCard offer={offer} onSelectOffer={handleSelectOffer} />
+            </Grid>
+          ))}
+        </Grid>
+      )}
 
       <Dialog open={openDialog} onClose={handleCloseDialog}>
         {selectedOffer && (
@@ -144,36 +219,34 @@ const OfferScreen = () => {
             <DialogContent>
               <Card sx={{ maxWidth: 345 }}>
                 <CardActionArea>
-                  <CardMedia
-                    component="img"
-                    height="140"
-                    image={selectedOffer.image}
-                    alt={selectedOffer.nom}
-                  />
+                  <CardMedia component="img" height="140" image={selectedOffer.image} alt={selectedOffer.nom} />
                   <CardContent>
                     <Typography gutterBottom variant="h5" component="div">
                       {selectedOffer.nom}
                     </Typography>
                     <Typography
-            variant="body2"
-            color="text.secondary"
-            sx={{
-              overflow: 'hidden',
-              textOverflow: 'ellipsis',
-              display: '-webkit-box',
-              WebkitLineClamp: expanded ? 'unset' : 4,
-              WebkitBoxOrient: 'vertical',
-            }}
-          >
-            {selectedOffer.description}
-          </Typography>
-          {selectedOffer.description.length > 180 && (
-            <Button color="primary" onClick={toggleDescription}>
-              {expanded ? 'Voir moins' : 'Voir plus'}
-            </Button>
-          )}
+                      variant="body3"
+                      color="text.secondary"
+                      sx={{
+                        overflow: 'hidden',
+                        textOverflow: 'ellipsis',
+                        display: '-webkit-box',
+                        WebkitLineClamp: expanded ? 'unset' : 4,
+                        WebkitBoxOrient: 'vertical',
+                      }}
+                    >
+                      {selectedOffer.description}
+                    </Typography>
+                    {selectedOffer.description.length > 180 && (
+                      <Button color="primary" onClick={toggleDescription}>
+                        {expanded ? 'Voir moins' : 'Voir plus'}
+                      </Button>
+                    )}
                     <Typography variant="h6" color="primary" sx={{ mt: 2 }}>
                       {selectedOffer.prix}€
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary" sx={{ mt: 1 }}>
+                      {selectedOffer.pays}/{selectedOffer.operateur}
                     </Typography>
                   </CardContent>
                 </CardActionArea>
